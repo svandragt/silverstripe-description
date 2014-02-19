856174bedc42e0fcf740cb0851b77f5ad6dce094
Sander van Dragt <sander@vandragt.com>
2014-02-19 11:07:17 +0000

    Don't attach extension by default

--- _config.php
+++ _config.php
@@ -1,6 +1,6 @@
 <?php
 
-// add to your dataobjects:
+// add to your dataobjects: 
 // http://api.silverstripe.org/trunk/sapphire/core/Object.html#methodadd_extension
 // Object::add_extension("DataObject","DescriptionDataExtension");
 
